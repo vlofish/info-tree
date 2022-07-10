78cd6f6a38b8b82680267d49bbb873da30d16054
vmijael <1945.github.m0dtb@slmail.me>
2022-07-10 01:04:35 -0500

    Excluding Two Tests due to Component Change

--- info-tree/src/app/app.component.spec.ts
+++ info-tree/src/app/app.component.spec.ts
@@ -20,13 +20,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'info-tree'`, () => {
+  xit(`should have as title 'info-tree'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('info-tree');
   });
 
-  it('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
